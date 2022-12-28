4514796e6d404c70a97cc77902f43ca50fa4f9b5
Steve The Stylua Robot <robot@example.com>
2022-12-28 19:35:40 +0000

    style: 💋 Auto-fix formatting using stylua 💋

--- lua/cheeky/snippets/cpp.lua
+++ lua/cheeky/snippets/cpp.lua
@@ -1,4 +1,4 @@
-local ls = require("luasnip")
+local ls = require "luasnip"
 local s = ls.snippet
 local sn = ls.snippet_node
 local isn = ls.indent_snippet_node
@@ -8,20 +8,20 @@ local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
-local events = require("luasnip.util.events")
-local ai = require("luasnip.nodes.absolute_indexer")
+local events = require "luasnip.util.events"
+local ai = require "luasnip.nodes.absolute_indexer"
 local fmt = require("luasnip.extras.fmt").fmt
 local m = require("luasnip.extras").m
 local lambda = require("luasnip.extras").l
 local parse = ls.parser.parse_snippet
 local rep = require("luasnip.extras").rep
 
-local common = require("cheeky.common")
+local common = require "cheeky.common"
 
 return {
-	parse(
-		"main",
-		[[#include <iostream>
+  parse(
+    "main",
+    [[#include <iostream>
 
 int main(int argc, char **argv) {
   using namespace std;
@@ -31,343 +31,343 @@ int main(int argc, char **argv) {
 
   return ret;
 }]]
-	),
+  ),
 
-	----------------------------------------------------------------------
-	--                             Printing                             --
-	----------------------------------------------------------------------
-	s("cout", fmt([[cout << "{}" << endl;]], { i(1, "somestuff") })),
-	s("cin", fmt([[cin >> {};]], { i(1, "variable") })),
+  ----------------------------------------------------------------------
+  --                             Printing                             --
+  ----------------------------------------------------------------------
+  s("cout", fmt([[cout << "{}" << endl;]], { i(1, "somestuff") })),
+  s("cin", fmt([[cin >> {};]], { i(1, "variable") })),
 
-	----------------------------------------------------------------------
-	--                            Shorthands                            --
-	----------------------------------------------------------------------
-	parse("c", "const"),
+  ----------------------------------------------------------------------
+  --                            Shorthands                            --
+  ----------------------------------------------------------------------
+  parse("c", "const"),
 
-	s("sc", fmt("static_cast<{}>({})", { i(1, "int"), i(2, "val") })),
-	parse("ca", "const auto"),
-	s("ce", fmt("constexpr {} {} = {};", { i(1, "auto"), i(2, "int"), i(3, "1") })),
+  s("sc", fmt("static_cast<{}>({})", { i(1, "int"), i(2, "val") })),
+  parse("ca", "const auto"),
+  s("ce", fmt("constexpr {} {} = {};", { i(1, "auto"), i(2, "int"), i(3, "1") })),
 
-	s(
-		"f",
-		fmt("[] []([]) { [] };", { i(1, "int"), i(2, "newfunc"), i(3, "int i"), i(4, "return i * 2;") }, {
-			delimiters = "[]",
-		})
-	),
+  s(
+    "f",
+    fmt("[] []([]) { [] };", { i(1, "int"), i(2, "newfunc"), i(3, "int i"), i(4, "return i * 2;") }, {
+      delimiters = "[]",
+    })
+  ),
 
-	----------------------------------------------------------------------
-	--                           preprocessor                           --
-	----------------------------------------------------------------------
-	-- Arm neon detection
-	s(
-		"ifneon",
-		fmt(
-			[[#ifdef __ARM_NEON__
+  ----------------------------------------------------------------------
+  --                           preprocessor                           --
+  ----------------------------------------------------------------------
+  -- Arm neon detection
+  s(
+    "ifneon",
+    fmt(
+      [[#ifdef __ARM_NEON__
 {}
 #endif
 		]],
-			{
-				i(1, "// ARM NEON Intrinsics here... "),
-			}
-		)
-	),
+      {
+        i(1, "// ARM NEON Intrinsics here... "),
+      }
+    )
+  ),
 
-	s(
-		"pragmamessage",
-		fmt([[#pragma message("{}") ]], {
-			i(1, "Hello!"),
-		})
-	),
+  s(
+    "pragmamessage",
+    fmt([[#pragma message("{}") ]], {
+      i(1, "Hello!"),
+    })
+  ),
 
-	----------------------------------------------------------------------
-	--                          STL Algorithms                          --
-	----------------------------------------------------------------------
+  ----------------------------------------------------------------------
+  --                          STL Algorithms                          --
+  ----------------------------------------------------------------------
 
-	-- All of
-	s(
-		"allof",
-		fmt([[std::all_of({}.cbegin(), {}.cend(), {});]], {
-			i(1, "v"),
-			rep(1),
-			i(2, "[](int i){ return i % 2 == 0; }"),
-		})
-	),
+  -- All of
+  s(
+    "allof",
+    fmt([[std::all_of({}.cbegin(), {}.cend(), {});]], {
+      i(1, "v"),
+      rep(1),
+      i(2, "[](int i){ return i % 2 == 0; }"),
+    })
+  ),
 
-	-- Any of
-	s(
-		"anyof",
-		fmt([[std::any_of({}.cbegin(), {}.cend(), {});]], {
-			i(1, "v"),
-			rep(1),
-			i(2, [[[](int i){ return i % 2 == 0; }]]),
-		})
-	),
+  -- Any of
+  s(
+    "anyof",
+    fmt([[std::any_of({}.cbegin(), {}.cend(), {});]], {
+      i(1, "v"),
+      rep(1),
+      i(2, [[[](int i){ return i % 2 == 0; }]]),
+    })
+  ),
 
-	-- None of
-	s(
-		"noneof",
-		fmt([[std::none_of({}.cbegin(), {}.cend(), {});]], {
-			i(1, "v"),
-			rep(1),
-			i(2, "[](int i){ return i % 2 == 0; }"),
-		})
-	),
+  -- None of
+  s(
+    "noneof",
+    fmt([[std::none_of({}.cbegin(), {}.cend(), {});]], {
+      i(1, "v"),
+      rep(1),
+      i(2, "[](int i){ return i % 2 == 0; }"),
+    })
+  ),
 
-	-- Find
-	s(
-		"find",
-		fmt([[std::find(begin({}), end({}), {});]], {
-			i(1, "v"),
-			rep(1),
-			i(2, "3"),
-		})
-	),
+  -- Find
+  s(
+    "find",
+    fmt([[std::find(begin({}), end({}), {});]], {
+      i(1, "v"),
+      rep(1),
+      i(2, "3"),
+    })
+  ),
 
-	-- Find if
-	s(
-		"findif",
-		fmt([[std::find_if(begin({}), end({}), {});]], {
-			i(1, "v"),
-			rep(1),
-			i(2, "[](int i){ return i%2 == 0; }"),
-		})
-	),
+  -- Find if
+  s(
+    "findif",
+    fmt([[std::find_if(begin({}), end({}), {});]], {
+      i(1, "v"),
+      rep(1),
+      i(2, "[](int i){ return i%2 == 0; }"),
+    })
+  ),
 
-	-- For each
-	s(
-		"foreach",
-		fmt([[std::for_each(begin({}), end({}), {});]], {
-			i(1, "v"),
-			rep(1),
-			i(2, "[](const int& n) { printf(n); }"),
-		})
-	),
+  -- For each
+  s(
+    "foreach",
+    fmt([[std::for_each(begin({}), end({}), {});]], {
+      i(1, "v"),
+      rep(1),
+      i(2, "[](const int& n) { printf(n); }"),
+    })
+  ),
 
-	-- Transform
-	s(
-		"transform",
-		fmt([[std::transform({}.cbegin(), {}.cend(), {}.begin(),  {});]], {
-			i(1, "v"),
-			rep(1),
-			rep(1),
-			i(2, "[](unsigned char c) { return std::toupper(c); }"),
-		})
-	),
+  -- Transform
+  s(
+    "transform",
+    fmt([[std::transform({}.cbegin(), {}.cend(), {}.begin(),  {});]], {
+      i(1, "v"),
+      rep(1),
+      rep(1),
+      i(2, "[](unsigned char c) { return std::toupper(c); }"),
+    })
+  ),
 
-	-- Copy
-	s(
-		"copy",
-		fmt([[std::copy({}.begin(), {}.end(), std::back_inserter({}));]], {
-			i(1, "from_vector"),
-			rep(1),
-			i(2, "to_vector"),
-		})
-	),
+  -- Copy
+  s(
+    "copy",
+    fmt([[std::copy({}.begin(), {}.end(), std::back_inserter({}));]], {
+      i(1, "from_vector"),
+      rep(1),
+      i(2, "to_vector"),
+    })
+  ),
 
-	-- Copy if
-	s(
-		"copyif",
-		fmt([[std::copy_if({}.begin(), {}.end(), std::back_inserter({}), {});]], {
-			i(1, "from_vector"),
-			rep(1),
-			i(2, "to_vector"),
-			i(3, "[](int x) { return x % 3 == 0; }"),
-		})
-	),
+  -- Copy if
+  s(
+    "copyif",
+    fmt([[std::copy_if({}.begin(), {}.end(), std::back_inserter({}), {});]], {
+      i(1, "from_vector"),
+      rep(1),
+      i(2, "to_vector"),
+      i(3, "[](int x) { return x % 3 == 0; }"),
+    })
+  ),
 
-	-- Remove
-	s(
-		"remove",
-		fmt([[std::remove({}.begin(), {}.end(), {});]], {
-			i(1, "from_vector"),
-			rep(1),
-			i(2, "2"),
-		})
-	),
+  -- Remove
+  s(
+    "remove",
+    fmt([[std::remove({}.begin(), {}.end(), {});]], {
+      i(1, "from_vector"),
+      rep(1),
+      i(2, "2"),
+    })
+  ),
 
-	-- Remove if
-	s(
-		"removeif",
-		fmt([[std::remove_if({}.begin(), {}.end(), {});]], {
-			i(1, "myvector"),
-			rep(1),
-			i(2, "[](int x) { return x % 3 == 0; }"),
-		})
-	),
-	-- Iota
-	s(
-		"iota",
-		fmt([[std::iota({}.begin(), {}.end(), {});]], {
-			i(1, "myvector"),
-			rep(1),
-			i(2, "4"),
-		})
-	),
-	-- Reverse
-	s(
-		"reverse",
-		fmt([[std::reverse({}.begin(), {}.end());]], {
-			i(1, "myvector"),
-			rep(1),
-		})
-	),
-	-- Sort
-	s(
-		"sort",
-		fmt([[std::sort({}.begin(), {}.end());]], {
-			i(1, "myvector"),
-			rep(1),
-		})
-	),
-	-- Shuffle
-	s(
-		"shuffle",
-		fmt(
-			[[std::random_device rd;
+  -- Remove if
+  s(
+    "removeif",
+    fmt([[std::remove_if({}.begin(), {}.end(), {});]], {
+      i(1, "myvector"),
+      rep(1),
+      i(2, "[](int x) { return x % 3 == 0; }"),
+    })
+  ),
+  -- Iota
+  s(
+    "iota",
+    fmt([[std::iota({}.begin(), {}.end(), {});]], {
+      i(1, "myvector"),
+      rep(1),
+      i(2, "4"),
+    })
+  ),
+  -- Reverse
+  s(
+    "reverse",
+    fmt([[std::reverse({}.begin(), {}.end());]], {
+      i(1, "myvector"),
+      rep(1),
+    })
+  ),
+  -- Sort
+  s(
+    "sort",
+    fmt([[std::sort({}.begin(), {}.end());]], {
+      i(1, "myvector"),
+      rep(1),
+    })
+  ),
+  -- Shuffle
+  s(
+    "shuffle",
+    fmt(
+      [[std::random_device rd;
 std::mt19937 g(rd());
 std::shuffle({}.begin(), {}.end(), {});]],
-			{
-				i(1, "myvector"),
-				rep(1),
-				i(2, "g"),
-			}
-		)
-	),
-	-- Sample
-	s(
-		"sample",
-		fmt(
-			[[std::random_device rd;
+      {
+        i(1, "myvector"),
+        rep(1),
+        i(2, "g"),
+      }
+    )
+  ),
+  -- Sample
+  s(
+    "sample",
+    fmt(
+      [[std::random_device rd;
 std::mt19937 g(rd());
 std::sample({}.begin(), {}.end(), {}, {}, {});]],
-			{
-				i(1, "myvector"),
-				rep(1),
-				i(2, "std::back_inserter(out)"),
-				i(3, "numSamples"),
-				i(4, "g"),
-			}
-		)
-	),
+      {
+        i(1, "myvector"),
+        rep(1),
+        i(2, "std::back_inserter(out)"),
+        i(3, "numSamples"),
+        i(4, "g"),
+      }
+    )
+  ),
 
-	-- Rotate
-	s(
-		"rotate",
-		fmt([[std::rotate({}.begin(), {}.end() + {}, {}.end());]], {
-			i(1, "myvector"),
-			rep(1),
-			i(2, "1"),
-			rep(3),
-		})
-	),
+  -- Rotate
+  s(
+    "rotate",
+    fmt([[std::rotate({}.begin(), {}.end() + {}, {}.end());]], {
+      i(1, "myvector"),
+      rep(1),
+      i(2, "1"),
+      rep(3),
+    })
+  ),
 
-	-- TODO
-	-- Equal
-	-- Mismatch
-	-- Previous permutation
-	-- Next permutation
-	-- Max element
-	-- Min element
-	-- Set_*
-	-- Partial sort
+  -- TODO
+  -- Equal
+  -- Mismatch
+  -- Previous permutation
+  -- Next permutation
+  -- Max element
+  -- Min element
+  -- Set_*
+  -- Partial sort
 
-	----------------------------------------------------------------------
-	--                         STL containers                           --
-	----------------------------------------------------------------------
-	-- See all here: https://en.cppreference.com/w/cpp/container
+  ----------------------------------------------------------------------
+  --                         STL containers                           --
+  ----------------------------------------------------------------------
+  -- See all here: https://en.cppreference.com/w/cpp/container
 
-	-- Map
-	s(
-		"map",
-		fmt([[std::map<[], []> []{{[], []}, {[], []}};]], {
-			i(1, "std::string"),
-			i(2, "int"),
-			i(3, "mapname"),
-			i(4, [["GPU"]]),
-			i(5, "5"),
-			i(6, [["CPU"]]),
-			i(7, "15"),
-		}, { delimiters = "[]" })
-	),
+  -- Map
+  s(
+    "map",
+    fmt([[std::map<[], []> []{{[], []}, {[], []}};]], {
+      i(1, "std::string"),
+      i(2, "int"),
+      i(3, "mapname"),
+      i(4, [["GPU"]]),
+      i(5, "5"),
+      i(6, [["CPU"]]),
+      i(7, "15"),
+    }, { delimiters = "[]" })
+  ),
 
-	s(
-		"unorderedmap",
-		fmt([[std::unordered_map<[], []> []{{[], []}, {[], []}};]], {
-			i(1, "std::string"),
-			i(2, "int"),
-			i(3, "mapname"),
-			i(4, [["GPU"]]),
-			i(5, "5"),
-			i(6, [["CPU"]]),
-			i(7, "15"),
-		}, { delimiters = "[]" })
-	),
+  s(
+    "unorderedmap",
+    fmt([[std::unordered_map<[], []> []{{[], []}, {[], []}};]], {
+      i(1, "std::string"),
+      i(2, "int"),
+      i(3, "mapname"),
+      i(4, [["GPU"]]),
+      i(5, "5"),
+      i(6, [["CPU"]]),
+      i(7, "15"),
+    }, { delimiters = "[]" })
+  ),
 
-	-- Array
-	s(
-		"arr",
-		fmt("std::array<{}, {}> {} = {};", {
-			i(1, "int"),
-			i(2, "3"),
-			i(3, "name"),
-			i(4, "{1,2,3}"),
-		})
-	),
+  -- Array
+  s(
+    "arr",
+    fmt("std::array<{}, {}> {} = {};", {
+      i(1, "int"),
+      i(2, "3"),
+      i(3, "name"),
+      i(4, "{1,2,3}"),
+    })
+  ),
 
-	-- Vector
-	s(
-		"vec",
-		fmt("std::vector<{}> {} = {};", {
-			i(1, "int"),
-			i(2, "name"),
-			i(3, "{1,2,3}"),
-		})
-	),
+  -- Vector
+  s(
+    "vec",
+    fmt("std::vector<{}> {} = {};", {
+      i(1, "int"),
+      i(2, "name"),
+      i(3, "{1,2,3}"),
+    })
+  ),
 
-	----------------------------------------------------------------------
-	--                            Functions                             --
-	----------------------------------------------------------------------
+  ----------------------------------------------------------------------
+  --                            Functions                             --
+  ----------------------------------------------------------------------
 
-	-- Lambda
-	s("lam", {
-		t("["),
-		i(1, "x"),
-		t("]"),
-		t("("),
-		i(2, "int y"),
-		t(")"),
-		t(" { "),
-		i(3, "return x * y > 55;"),
-		t(" }"),
-	}),
+  -- Lambda
+  s("lam", {
+    t "[",
+    i(1, "x"),
+    t "]",
+    t "(",
+    i(2, "int y"),
+    t ")",
+    t " { ",
+    i(3, "return x * y > 55;"),
+    t " }",
+  }),
 
-	----------------------------------------------------------------------
-	--                        Control structures                        --
-	----------------------------------------------------------------------
-	-- If else
-	s(
-		"ifelse",
-		fmt(
-			[[if([])
+  ----------------------------------------------------------------------
+  --                        Control structures                        --
+  ----------------------------------------------------------------------
+  -- If else
+  s(
+    "ifelse",
+    fmt(
+      [[if([])
 	{[]}
 else
 	{[]};]],
-			{
-				i(1, "condition"),
-				i(2, "true"),
-				i(3, "false"),
-			},
-			{
-				delimiters = "[]",
-			}
-		)
-	),
+      {
+        i(1, "condition"),
+        i(2, "true"),
+        i(3, "false"),
+      },
+      {
+        delimiters = "[]",
+      }
+    )
+  ),
 
-	-- Class
-	s(
-		"class",
-		fmt(
-			[[
+  -- Class
+  s(
+    "class",
+    fmt(
+      [[
 #pragma once
 #ifndef []
 #define []
@@ -391,61 +391,61 @@ private:
 } // namespace []
 #endif
 ]],
-			{
+      {
 
-				-- d(6, function(_,_) return i(nil, string.upper(fileNoExt())) end),
-				common.ifilenameNoExtUpper(6),
-				rep(6),
+        -- d(6, function(_,_) return i(nil, string.upper(fileNoExt())) end),
+        common.ifilenameNoExtUpper(6),
+        rep(6),
 
-				i(1, "mk"),
-				-- Use file name to set class
-				common.ifilenameNoExtUpper(2),
-				i(3, "Mads Kjeldgaard"),
-				i(4, os.date("%d-%m-%Y")),
-				i(5, "longer description"),
-				rep(2),
-				rep(2),
-				rep(2),
-				rep(1),
-			},
-			{
-				delimiters = "[]",
-			}
-		)
-	),
+        i(1, "mk"),
+        -- Use file name to set class
+        common.ifilenameNoExtUpper(2),
+        i(3, "Mads Kjeldgaard"),
+        i(4, os.date "%d-%m-%Y"),
+        i(5, "longer description"),
+        rep(2),
+        rep(2),
+        rep(2),
+        rep(1),
+      },
+      {
+        delimiters = "[]",
+      }
+    )
+  ),
 
-	----------------------------------------------------------------------
-	--                           Preprocessor                           --
-	----------------------------------------------------------------------
+  ----------------------------------------------------------------------
+  --                           Preprocessor                           --
+  ----------------------------------------------------------------------
 
-	-- #ifndef
-	s(
-		"ifndef",
-		fmt(
-			[[#pragma once
+  -- #ifndef
+  s(
+    "ifndef",
+    fmt(
+      [[#pragma once
 #ifndef {}
 #define {}
 
 #endif
 		]],
-			{ i(1, "YO"), rep(1) }
-		)
-	),
+      { i(1, "YO"), rep(1) }
+    )
+  ),
 
-	----------------------------------------------------------------------
-	--                          Supercollider                           --
-	----------------------------------------------------------------------
+  ----------------------------------------------------------------------
+  --                          Supercollider                           --
+  ----------------------------------------------------------------------
 
-	-- Supercollider specific stuff
-	s("rtalloc", {
-		t("("),
-		i(1, "float"),
-		t("*)RTAlloc("),
-		i(2, "unit->mWorld"),
-		t(","),
-		i(3, "unit->bufsize"),
-		t("* sizeof("),
-		i(4, "float"),
-		t(")"),
-	}),
+  -- Supercollider specific stuff
+  s("rtalloc", {
+    t "(",
+    i(1, "float"),
+    t "*)RTAlloc(",
+    i(2, "unit->mWorld"),
+    t ",",
+    i(3, "unit->bufsize"),
+    t "* sizeof(",
+    i(4, "float"),
+    t ")",
+  }),
 }
